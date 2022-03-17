149d8972958053e64524c9cac79e14f85c59f916
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-03-17 02:04:49 -0500

    modulo agregado

--- mi_academia/__manifest__.py
+++ mi_academia/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mi_modulo",
+    'name': "mi_academia",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,14 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "RonaldinhoTEC",
+    'author': "ronaldinhoTEC",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'mis_modulos',
-    'version': '1.0',
+    'category': 'Mis Modulos',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
