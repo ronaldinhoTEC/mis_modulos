9af2dc2d43606419239b2d2b367bbc7f45fc6b72
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-03-18 01:10:55 -0500

    c_3

--- bloquetas/models/models.py
+++ bloquetas/models/models.py
@@ -6,10 +6,10 @@ from odoo import models, fields, api
 class Propiedades(models.Model):
     _name = 'bloquetas.propiedades'
 
-    nombre = fields.Char(string="Nombre:", required=True)
-    medidas = fields.Char(string="Medidas:", required=True)
-    cantidad = fields.Integer(string="Cantidad:", required=True)
-    presupuesto = fields.Text(string="presupuesto:", required=True)
+    nombre = fields.Char("Nombre", required=True)
+    medidas = fields.Char("Medidas:", required=True)
+    cantidad = fields.Integer("Cantidad:", required=True)
+    presupuesto = fields.Text("presupuesto:", required=True)
     tipo = fields.Selection([('casa', 'Casa'), ('edificio', 'Edificio')], string='Tipo de bloqueta')
     fecha = fields.Date()
 #     @api.depends('value')
