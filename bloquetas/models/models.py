674a81a86f832ad3b97a56a64611b44cc072720d
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-03-19 01:39:22 -0500

    a

--- bloquetas/models/models.py
+++ bloquetas/models/models.py
@@ -13,14 +13,7 @@ class Propiedades(models.Model):
     tipo = fields.Selection([('casa', 'Casa'), ('edificio', 'Edificio')], string='Tipo de bloqueta')
     fecha = fields.Date()
     
-class Type(models.Model):
-    
-    _inherit = 'sale.order'
-    
-    partner_id = fields.Many2one(
-        'bloquetas.propiedades',
-        string='partner',
-        )
+
 
 
 
