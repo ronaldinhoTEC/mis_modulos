ce1183ad442449f6849e0da002bd37ce515c5c96
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-03-28 01:41:12 -0500

    a

--- bloquetas/models/models.py
+++ bloquetas/models/models.py
@@ -13,11 +13,7 @@ class Propiedades(models.Model):
     tipo = fields.Selection([('casa', 'Casa'), ('edificio', 'Edificio')], string='Tipo de bloqueta')
     fecha = fields.Date()
     age_range_id = fields.Many2one(
-        "res.partner.age.range",
-        "Age Range",
-        compute="_compute_age_range_id",
-        store=True,
-    )
+        "res.partner.age.range", string="Age Range", required=True)
     
 
 
